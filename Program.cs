8c427bfaafaa99a2e7d12085665b5df8c0385427
pundalik Thogam <pthogam@gmail.com>
2023-03-17 11:25:54 +0530

    file added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
